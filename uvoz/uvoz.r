c6f2af11e2e1f85bf2b79b609d67400e7ccf0351
Nadja <legan.nadja83@gmail.com>
2015-12-22 21:29:59 +0100

    Uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,20 +1,44 @@
-# 2. faza: Uvoz podatkov
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+#prihodi in prenočitve tujih ter domačih turistov po nastanitvenih objektih za januar 2015
+uvozi.nastobjJanuar <- function() {
+  return(read.csv2("podatki/VrsteNastanitvenihObjektovJanuar.csv", sep = ";", as.is = TRUE,
+                   #row.names = 1,
+                   skip=4,
+                   col.names = c("Objekti","Prihodi_turistov_domači","Prihodi_turistov_tuji","Prenočitve_turistov-domači","Prenočitve_turistov-tuji"),
+                   fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
 
-obcine <- uvozi.obcine()
+nastobjJanuar <- uvozi.nastobjJanuar()
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+#prihodi in prenočitve tujih ter domačih turistov po nastanitvenih objektih za avgust 2015
+uvozi.nastobjAvgust <- function() {
+  return(read.csv2("podatki/VrsteNastanitvenihObjektovAvgust.csv", sep = ";", as.is = TRUE,
+                   row.names = 1,
+                   skip=4,
+                   col.names = c("Objekti","Prihodi_turistov_domači","Prihodi_turistov_tuji","Prenočitve_turistov_domači","Prenočitve_turistov_tuji"),
+                   fileEncoding = "Windows-1250"))
+}
+
+nastobjAvgust <- uvozi.nastobjAvgust()
+
+#prihodi in prenočitve tujih ter domačih turistov po občinah za avgust 2015
+uvozi.obAvgust <- function() {
+  return(read.csv2("podatki/obcineAvgust.csv", sep = ";", as.is = TRUE,
+                   row.names = 1,
+                   skip = 3,
+                   col.names = c("Obcine", "Prihodi_domaci", "Prihodi_tuji", "Prenocitve_domaci", "Prenocitve_tuji"),
+                   fileEncoding = "Windows-1250"))
+}
+
+obAvgust <- uvozi.obAvgust()
+
+#prihodi in prenočitve tujih ter domačih turistov po občinah za avgust 2015
+uvozi.obJanuar <- function() {
+  return(read.csv2("podatki/obcineJanuar.csv", sep = ";", as.is = TRUE,
+                   row.names = 1,
+                   skip = 3,
+                   col.names = c("Obcine", "Prihodi_domaci", "Prihodi_tuji", "Prenocitve_domaci", "Prenocitve_tuji"),
+                   fileEncoding = "Windows-1250"))
+}
+obJanuar <- uvozi.obJanuar()
+
